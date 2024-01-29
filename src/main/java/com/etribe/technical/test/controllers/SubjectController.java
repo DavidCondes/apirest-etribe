a0935299dea02d77208390cb01a60177a50eb98c
David Alejandro Condes <63083227+DavidCondes@users.noreply.github.com>
2024-01-29 14:46:30 -0600

    Update SubjectController.java

--- src/main/java/com/etribe/technical/test/controllers/SubjectController.java
+++ src/main/java/com/etribe/technical/test/controllers/SubjectController.java
@@ -4,6 +4,7 @@ package com.etribe.technical.test.controllers;
 import com.etribe.technical.test.models.SubjectModel;
 import com.etribe.technical.test.services.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,26 +20,31 @@ public class SubjectController {
 
 
     @PostMapping("/add-subject")
+    @ResponseStatus(HttpStatus.CREATED)
     public SubjectModel saveSubject(@RequestBody SubjectModel subject){
         return subjectService.saveOrUpdate(subject);
     }
 
     @GetMapping("/get-all")
+    @ResponseStatus(HttpStatus.OK)
     public ArrayList<SubjectModel> getAllSubjects() {
         return subjectService.getAll();
     }
 
     @GetMapping("/get-by-id")
+    @ResponseStatus(HttpStatus.OK)
     public Optional<SubjectModel> getSubjectId(@RequestParam(name = "id") Long id) {
         return subjectService.getById(id);
     }
 
     @PostMapping("/update")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public SubjectModel updateSubject(@RequestBody SubjectModel subject){
         return subjectService.saveOrUpdate(subject);
     }
 
     @GetMapping("/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteSubject(@RequestParam(name = "id") Long id){
         this.subjectService.delete(id);
     }
